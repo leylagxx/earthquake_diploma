1cf723fc937ae2e3bad0799481b7308264201008
QuanyshK <zoomer.qurbany@gmail.com>
2024-06-05 04:07:33 +0500

    Display data from API

--- app/src/main/java/com/example/earthquakeqazaqedition/data/network/ApiService.kt
+++ app/src/main/java/com/example/earthquakeqazaqedition/data/network/ApiService.kt
@@ -2,9 +2,11 @@ package com.example.earthquakeqazaqedition.data.network
 
 import com.example.earthquakeqazaqedition.data.model.EarthquakeResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("summary/4.5_day.geojson")
-    suspend fun fetchEarthquakes(): EarthquakeResponse
+    @GET("summary/all_hour.geojson")
+    suspend fun fetchEarthquakes(
+    ): EarthquakeResponse
 
 }
--- app/src/main/java/com/example/earthquakeqazaqedition/presentation/adapter/EarthquakeAdapter.kt
+++ app/src/main/java/com/example/earthquakeqazaqedition/presentation/adapter/EarthquakeAdapter.kt
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.ListAdapter
 import com.example.earthquakeqazaqedition.R
 import com.example.earthquakeqazaqedition.databinding.EarthquakeItemBinding
 import com.example.earthquakeqazaqedition.data.model.Earthquake
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class EarthquakeAdapter : ListAdapter<Earthquake, EarthquakeAdapter.ViewHolder>(EarthquakeDiffUtil()) {
+class EarthquakeAdapter : ListAdapter<Earthquake, RecyclerView.ViewHolder>(EarthquakeDiffUtil()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -20,17 +23,26 @@ class EarthquakeAdapter : ListAdapter<Earthquake, EarthquakeAdapter.ViewHolder>(
         )
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewHolder){
+            return holder.bind(getItem(position))
+        }
     }
+
+
 
     inner class ViewHolder(private val binding: EarthquakeItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         private val context = binding.root.context
+
         fun bind(earthquake: Earthquake) {
             with(binding) {
-                magnituda.text = earthquake.properties.mag.toString()
+                magnituda.text = "M ${earthquake.properties.mag.toString()}"
                 location.text = earthquake.properties.place
+                val calendar = Calendar.getInstance()
+                calendar.timeInMillis = earthquake.properties.time
+                val format = SimpleDateFormat("E, MMM d, yyyy  hh:mm", Locale.getDefault())
+                time.text = format.format(calendar.time)
             }
         }
     }
